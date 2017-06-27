3f7f57ae57a5d4377e20f4e0544e8436bec905ee
zihun130 <zihun130@126.com>
2017-06-27 19:13:56 +0800

    集成投资管理线形图

--- app/src/main/java/atguigu/com/p2pfinancial0224/activity/TouZiActivity.java
+++ app/src/main/java/atguigu/com/p2pfinancial0224/activity/TouZiActivity.java
@@ -1,9 +1,43 @@
 package atguigu.com.p2pfinancial0224.activity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 import atguigu.com.p2pfinancial0224.R;
 import atguigu.com.p2pfinancial0224.base.BaseActivity;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class TouZiActivity extends BaseActivity {
+
+    @InjectView(R.id.base_title)
+    TextView baseTitle;
+    @InjectView(R.id.base_back)
+    ImageView baseBack;
+    @InjectView(R.id.base_setting)
+    ImageView baseSetting;
+    @InjectView(R.id.chart)
+    LineChart chart;
+
+    @Override
+    public void initTitle() {
+        super.initTitle();
+        baseTitle.setText("投资管理");
+        baseBack.setVisibility(View.VISIBLE);
+    }
 
     @Override
     public void initListener() {
@@ -13,6 +47,32 @@ public class TouZiActivity extends BaseActivity {
     @Override
     public void initData() {
 
+        Typeface mTf = Typeface.createFromAsset(this.getAssets(), "OpenSans-Regular.ttf");
+
+        chart.setDescription("");
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTypeface(mTf);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTypeface(mTf);
+        leftAxis.setLabelCount(5, false);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setTypeface(mTf);
+        rightAxis.setLabelCount(5, false);
+        rightAxis.setDrawGridLines(false);
+
+        // set data
+        chart.setData((LineData) generateDataLine(1));
+
+        // do not forget to refresh the chart
+        // chart.invalidate();
+        chart.animateX(750);
     }
 
     @Override
@@ -23,5 +83,67 @@ public class TouZiActivity extends BaseActivity {
     @Override
     public int getLayoutId() {
         return R.layout.activity_tou_zi;
+    }
+    
+    @OnClick(R.id.base_back)
+    public void onViewClicked() {
+        finish();
+    }
+
+
+
+    private LineData generateDataLine(int cnt) {
+
+        ArrayList<Entry> e1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e1.add(new Entry((int) (Math.random() * 65) + 40, i));
+        }
+
+        LineDataSet d1 = new LineDataSet(e1, "New DataSet " + cnt + ", (1)");
+        d1.setLineWidth(2.5f);
+        d1.setCircleSize(4.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> e2 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e2.add(new Entry(e1.get(i).getVal() - 30, i));
+        }
+
+        LineDataSet d2 = new LineDataSet(e2, "New DataSet " + cnt + ", (2)");
+        d2.setLineWidth(2.5f);
+        d2.setCircleSize(4.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<LineDataSet> sets = new ArrayList<LineDataSet>();
+        sets.add(d1);
+        sets.add(d2);
+
+        LineData cd = new LineData(getMonths(), sets);
+        return cd;
+    }
+
+    private ArrayList<String> getMonths() {
+
+        ArrayList<String> m = new ArrayList<String>();
+        m.add("Jan");
+        m.add("Feb");
+        m.add("Mar");
+        m.add("Apr");
+        m.add("May");
+        m.add("Jun");
+        m.add("Jul");
+        m.add("Aug");
+        m.add("Sep");
+        m.add("Okt");
+        m.add("Nov");
+        m.add("Dec");
+
+        return m;
     }
 }
